e0657dc7587831db8bd2ea037eed0b93ee206fe3
HubbleCommand <sasha.poirier@gmail.com>
2021-01-22 10:25:52 +0000

    Better logging

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var nodemailer = require('nodemailer'); 
-var http = require('http');
+var nodemailer = require('nodemailer');
 var axios = require('axios');
 var setup = require('./setup');
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+//It may be required to put the following line at the very top: /usr/bin/env node (add #! before /usr/...)
 
 function sendWarningMail(sender, reciever, subject, message){
     var transporter = nodemailer.createTransport({
@@ -28,7 +33,15 @@ function sendWarningMail(sender, reciever, subject, message){
     }); 
 }
 
-console.log("Running Site Down Detector with the following settings...")
+function printCurrentTimeLogFormat(){
+    let d = new Date();
+    return monthNames[d.getMonth()].substring(0, 3) + ' ' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+}
+
+console.log(' ')
+console.log(' ------------------------------ ')
+console.log()
+console.log(printCurrentTimeLogFormat() + " Running Site Down Detector with the following settings...")
 console.log(setup);
 
 for(let sItem of setup.watchers){
@@ -38,17 +51,13 @@ for(let sItem of setup.watchers){
                 timeout: sItem.timeout
             })
             .then(() => {
-
+                console.log('Site appears to be online');
             })
             .catch((err) => {
-                console.log(err.code)
-                console.log(err.message)
+                console.log(err.code + ':' + err.message);
 
                 if(err.code == 'ECONNABORTED'){
-                    console.log('TIMEOUT EXCEEDED FOR SITE ' + url + ', sending warning email')
-
-                    console.log(sItem.sender)
-
+                    console.log('TIMEOUT EXCEEDED FOR SITE ' + url + ', sending warning email');
                     for(let reciever of sItem.recievers){
                         sendWarningMail(
                             {
@@ -62,7 +71,7 @@ for(let sItem of setup.watchers){
                         )
                     }
                 }
-            })
+            });
         
     }
     
